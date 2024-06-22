a254078dc1f16943a1fec0204eb63a85f4f16c81
daucu <info@harshaweb.com>
2024-06-22 09:31:11 +0530

    chore: Remove Daucu Scheduler from console layout

--- frontend/src/app/console/(tempates)/mysql/page.jsx
+++ frontend/src/app/console/(tempates)/mysql/page.jsx
@@ -143,12 +143,12 @@ export default function Page(params) {
 
   return (
     <div>
-      <div className="flex w-auto h-full flex-col space-y-2">
+      <div className="flex w-auto h-full flex-col">
         <div className="bg-gray-100 border-l-[5px] border-blue-600 dark:border-gray-500 dark:bg-slate-900 p-2 grid grid-cols-10">
           {/* Heading */}
           <div className="flex flex-col justify-between items-start col-span-8">
             <h1 className="text-xl font-bold dark:text-gray-400 text-black">
-              MySQL database Cluster
+              MySQL database
             </h1>
             <span className="text dark:text-gray-400 text-black text-xs">
               A Managed MySQL Database service simplifies MySQL database hosting
@@ -193,79 +193,63 @@ export default function Page(params) {
                       </p>
                     </div>
                   ) : (
-                    <div>
-                      {/* Filter  */}
-                      <div className="flex justify-between">
-                        <input placeholder="Filter..." className="input rounded-sm input-sm input-bordered" />
-                        <button className="btn-sm btn rounded-sm">
-                          View
-                        </button>
-                      </div>
-                      <div className="overflow-x-auto mt-5">
-                        <table className="table table-sm">
-                          {/* head */}
-                          <thead>
-                            <tr className="dark:bg-slate-900 rounded-sm bg-white dark:text-gray-400 text-black">
-                              <th>#</th>
-                              <th>Cluster Name</th>
-                              <th>MySQL Version</th>
-                              <th>Hight Availability</th>
-                              <th>Created AT</th>
-                              <th>Action</th>
-                            </tr>
-                          </thead>
-                          <tbody className="mt-5 dark:text-gray-400 text-black">
-                            {/* Map Disk */}
-                            {clusters &&
-                              clusters !== null &&
-                              clusters?.map((cluster, index) => (
-                                <tr
-                                  key={index}
-                                  className="dark:bg-slate-900 rounded-sm bg-white"
+                    <div className="grid gap-2 grid-cols-2 2xl:grid-cols-6 xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 select-none">
+                      {clusters &&
+                        clusters.map((item, index) => (
+                          <div
+                            key={index}
+                            // whileTap={{ scale: 0.9 }}
+                            // style={{ x, opacity }}
+                            className="inline-flex border-blue-600 justify-between items-center p-3 w-full text-gray-500 cursor-pointer dark:hover:text-gray-300 dark:border-gray-500 border-l-4 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-slate-700 dark:hover:bg-gray-700 bg-slate-200 rounded-sm container z-40"
+                            onClick={() => {
+                              router.push(
+                                `/console/mysql/details?tab=information&label=${item.namespace}`,
+                                {
+                                  scroll: false,
+                                }
+                              );
+                            }}
+                          >
+                            <div className="cursor-pointer flex justify-between text-center items-center overflow-hidden w-full">
+                              <div className="h-[40px] w-[40px] rounded-md image-full bg-slate-200 p-1 mr-2 flex items-center justify-center">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="25"
+                                  height="25"
+                                  fill="currentColor"
+                                  class="bi bi-database fill-slate-800"
+                                  viewBox="0 0 16 16"
                                 >
-                                  <th>{index + 1}</th>
-                                  <td>{cluster?.name}</td>
-                                  <td>{cluster?.mysql_version}</td>
-                                  <td>
-                                    {cluster?.hight_availability ? (
-                                      <span className="text-green-500">
-                                        Enabled
-                                      </span>
-                                    ) : (
-                                      <span className="text-red-500">
-                                        Disabled
-                                      </span>
-                                    )}
-                                  </td>
-                                  <td>
-                                    {/* {cluster?.createdAt} */}
-                                    {formatTimeAgo(cluster?.updatedAt)}
-                                  </td>
-                                  <td className="space-x-1">
-                                    <button
-                                      className="btn btn-xs rounded-sm btn-error no-animation"
-                                      onClick={() => {
-                                        deleteCluster(cluster?._id);
-                                      }}
-                                    >
-                                      Delete
-                                    </button>
-                                    <button
-                                      className="btn btn-xs rounded-sm btn-neutral no-animation"
-                                      onClick={() => {
-                                        router.push(
-                                          `/console/mysql/details?tab=information&label=${cluster.namespace}`
-                                        );
-                                      }}
-                                    >
-                                      View &rarr;
-                                    </button>
-                                  </td>
-                                </tr>
-                              ))}
-                          </tbody>
-                        </table>
-                      </div>
+                                  <path d="M4.318 2.687C5.234 2.271 6.536 2 8 2s2.766.27 3.682.687C12.644 3.125 13 3.627 13 4c0 .374-.356.875-1.318 1.313C10.766 5.729 9.464 6 8 6s-2.766-.27-3.682-.687C3.356 4.875 3 4.373 3 4c0-.374.356-.875 1.318-1.313M13 5.698V7c0 .374-.356.875-1.318 1.313C10.766 8.729 9.464 9 8 9s-2.766-.27-3.682-.687C3.356 7.875 3 7.373 3 7V5.698c.271.202.58.378.904.525C4.978 6.711 6.427 7 8 7s3.022-.289 4.096-.777A5 5 0 0 0 13 5.698M14 4c0-1.007-.875-1.755-1.904-2.223C11.022 1.289 9.573 1 8 1s-3.022.289-4.096.777C2.875 2.245 2 2.993 2 4v9c0 1.007.875 1.755 1.904 2.223C4.978 15.71 6.427 16 8 16s3.022-.289 4.096-.777C13.125 14.755 14 14.007 14 13zm-1 4.698V10c0 .374-.356.875-1.318 1.313C10.766 11.729 9.464 12 8 12s-2.766-.27-3.682-.687C3.356 10.875 3 10.373 3 10V8.698c.271.202.58.378.904.525C4.978 9.71 6.427 10 8 10s3.022-.289 4.096-.777A5 5 0 0 0 13 8.698m0 3V13c0 .374-.356.875-1.318 1.313C10.766 14.729 9.464 15 8 15s-2.766-.27-3.682-.687C3.356 13.875 3 13.373 3 13v-1.302c.271.202.58.378.904.525C4.978 12.71 6.427 13 8 13s3.022-.289 4.096-.777c.324-.147.633-.323.904-.525" />
+                                </svg>
+                              </div>
+                              <div className="flex flex-col justify-start w-full items-start text-left">
+                                <div className="flex items-center justify-center"></div>
+                                <div
+                                  className="w-[100%] relative text-lg truncate dark:text-gray-400 text-black"
+                                  style={{
+                                    whiteSpace: "nowrap",
+                                    overflow: "hidden",
+                                    textOverflow: "ellipsis",
+                                  }}
+                                >
+                                  {item?.name}
+                                </div>
+                                <div
+                                  className="w-full text-xs overflow-ellipsis dark:text-gray-400 text-black"
+                                  style={{
+                                    WebkitLineClamp: 2,
+                                    WebkitBoxOrient: "vertical",
+                                    display: "-webkit-box",
+                                    overflow: "hidden",
+                                  }}
+                                >
+                                  {item?.createdAt}
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        ))}
                     </div>
                   )}
                 </div>
