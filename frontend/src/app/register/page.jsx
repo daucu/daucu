c793701b13dbb337cd2c2bce1a4ace0c74996c96
daucu <info@harshaweb.com>
2024-06-28 20:02:31 +0530

    chore: Update Register page to set default country as "none"
    This commit updates the Register page to set the default country value as "none" instead of the previously selected country. This change improves the user experience by providing a clear indication that no country is selected by default.
    
    Fixes #<issue_number>

--- frontend/src/app/register/page.jsx
+++ frontend/src/app/register/page.jsx
@@ -22,7 +22,7 @@ export default function Register() {
         {
           fullname: fullname,
           email: email,
-          country: country,
+          country: "none",
           phone: phone,
           username: username,
           password: password,
@@ -136,29 +136,6 @@ export default function Register() {
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 input"
                   required=""
                 />
-              </div>
-              <div>
-                <label
-                  htmlFor="country"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your Country
-                </label>
-                <select
-                  type="text"
-                  name="country"
-                  id="country"
-                  onChange={(e) => {
-                    setCountry(e.target.value);
-                  }}
-                  value={country}
-                  placeholder="INDIA"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 input"
-                  required=""
-                >
-                  <option value="india" name="india">India</option>
-                  <option value="usa" name="usa">India</option>
-                </select>
               </div>
               <div>
                 <label
